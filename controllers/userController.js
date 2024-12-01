58b065f5d6f6700217fb671554cc84706c472f62
Anton Kovachev <ant.kovachev@gmail.com>
2024-12-01 23:18:33 +0200

    Fix bug with controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,14 @@
-// controllers/userController.js
 const User = require('../models/User');
 
-exports.showRegisterPage = (req, res) => {
-    res.sendFile(path.join(__dirname, 'register.html'));
-};
-
-exports.registerUser = async (req, res) => {
+exports.register = async (req, res) => {
     const { username, email, password } = req.body;
+
     try {
         const existingUser = await User.findOne({ $or: [{ username }, { email }] });
         if (existingUser) {
             return res.status(400).send('User already exists');
         }
+
         const user = new User({ username, email, password });
         await user.save();
         res.status(201).send('User registered successfully');
@@ -21,21 +18,20 @@ exports.registerUser = async (req, res) => {
     }
 };
 
-exports.showLoginPage = (req, res) => {
-    res.sendFile(path.join(__dirname, 'login.html'));
-};
-
-exports.loginUser = async (req, res) => {
+exports.login = async (req, res) => {
     const { email, password } = req.body;
+
     try {
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(400).send('User not found');
         }
+
         const isMatch = await user.comparePassword(password);
         if (!isMatch) {
             return res.status(400).send('Invalid credentials');
         }
+
         res.status(200).send('Login successful');
     } catch (err) {
         console.error('Error logging in:', err);
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const app = express();
-const PORT = 3000;
 const taskController = require('./controllers/taskController');
 const userController = require('./controllers/userController');
+
+const app = express();
+const PORT = 3000;
 
 mongoose.connect('mongodb://localhost:27017/to_do_app');
 
@@ -17,15 +18,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/tasks', taskController.getTasks);
-app.delete('/delete/:id', taskController.deleteTask);
-app.put('/update/:id', taskController.updateTask);
 app.post('/submit', taskController.createTask);
+app.put('/update/:id', taskController.updateTask);
+app.delete('/delete/:id', taskController.deleteTask);
 
-app.get('/register', userController.showRegisterPage);
-app.post('/register', userController.registerUser);
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'register.html'));
+});
+app.post('/register', userController.register);
 
-app.get('/login', userController.showLoginPage);
-app.post('/login', userController.loginUser);
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+app.post('/login', userController.login);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
